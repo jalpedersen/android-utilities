98455d34bcf0de19a6d0c09f3d51d7136cccdfa0
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2018-10-16 12:06:53 +0200

    crop directly when taking screenshot

--- take_screenshot.sh
+++ take_screenshot.sh
@@ -3,4 +3,9 @@ if [ -z $1 ]; then
     echo missing output
     exit 1
 fi
-adb shell screencap -p  > $1
+if [ "$NO_CROP" == "1" ]; then
+    adb shell screencap -p  > $1
+else
+    top=${CROP_TOP:-70}
+    adb shell screencap -p | convert -gravity North -chop 0x$top - $1
+fi
